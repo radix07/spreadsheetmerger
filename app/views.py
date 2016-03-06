9b68fa44d76ab60a4282f33676dd2f77b84e2767
radix07 <rsatterleeniu@gmail.com>
2016-03-06 16:44:59 -0600

    fix file upload path

--- app/views.py
+++ app/views.py
@@ -6,15 +6,50 @@ Licence: GPLv3
 import os
 from werkzeug import secure_filename
 
-from flask import url_for, redirect, render_template, flash, g, session
+from flask import url_for, request, redirect, render_template, flash, g, session, send_from_directory
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, lm
 from forms import ExampleForm, LoginForm
 from models import User
 
-UPLOAD_FOLDER = 'tmp'
-ALLOWED_EXTENSIONS = set(['csv', 'xls', 'png', 'jpg', 'jpeg', 'gif'])
+#OS environment to pick directory
+UPLOAD_FOLDER = '/home/ryan/tmp'
+app.config['ALLOWED_EXTENSIONS'] = set(['txt','csv', 'xls', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+               filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    print "Upload function"
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        print os.path
+        print app.config['UPLOAD_FOLDER']
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file', filename=filename))
+    else:
+        print "invalid file type, handle error"
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    print "Uploaded file"
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 @app.route('/')
 def index():
